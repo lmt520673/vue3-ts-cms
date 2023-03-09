13f4557b0e284aeaa52127be789fbe11ca637867
limengting <1332901770@qq.com>
2023-03-09 19:38:52 +0800

    引入ele

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
     <router-link to="login">登录</router-link>
     <router-link to="main">主页</router-link>
 
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+
     <router-view></router-view>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import pinia from './store'
+
 import 'normalize.css'
 import '@/assets/css/index.less'
-import router from './router'
-import store from './store'
+
+//全局引入  方便（但是占用空间太大）
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+// app.use(ElementPlus)
 
 const app = createApp(App)
 app.use(router)
-app.use(store)
+app.use(pinia)
 app.mount('#app')
